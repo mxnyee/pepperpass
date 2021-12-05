dff9c6d2eee6646bbf094e0a177b35f63b0d249c
Maxine Yee <maxineyeee@gmail.com>
2021-12-04 16:41:28 -0800

    login page

---
+++ src/crypto/login.js
@@ -0,0 +1,25 @@
+const ACCOUNT_EMAIL = 'snowden@example.com';
+const ACCOUNT_PASS =
+  '5c47970310c7f2746caeea16fbb69f46962207832813ecfcc9269103c0bf0ab6';
+
+// https://remarkablemark.medium.com/how-to-generate-a-sha-256-hash-with-javascript-d3b2696382fd
+async function hash(string) {
+  const utf8 = new TextEncoder().encode(string);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((bytes) => bytes.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex;
+}
+
+export const accountLogin = async (email, passphrase) => {
+  try {
+    const hashedPassphrase = await hash(passphrase);
+    console.log({ email, passphrase, hashedPassphrase });
+    return email === ACCOUNT_EMAIL && hashedPassphrase === ACCOUNT_PASS;
+  } catch (e) {
+    console.log('Unable to login.');
+    return false;
+  }
+};
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useState } from 'react';
 import './Popup.css';
 
+import { accountLogin } from '../../crypto/login';
+
+const Login = ({ onLogin }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [text, setText] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const success = await accountLogin(email, password);
+    if (success) {
+      onLogin();
+    } else {
+      setText('Invalid login credentials.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <br />
+      <input
+        id="email"
+        name="email"
+        type="text"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+          setText('');
+        }}
+      />
+      <br />
+      <br />
+      <label htmlFor="masterpass">Master Password</label>
+      <br />
+      <input
+        id="masterpass"
+        name="password"
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+          setText('');
+        }}
+      />
+      <br />
+      <button>Log in</button>
+      <br />
+      <span>{text}</span>
+    </form>
+  );
+};
+
+const Account = ({ onLogout }) => {
+  return <button onClick={onLogout}>Log out</button>;
+};
+
 const Popup = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <h6 className="App-header">PepperPass</h6>
+      {loggedIn ? (
+        <Account onLogout={() => setLoggedIn(false)} />
+      ) : (
+        <Login onLogin={() => setLoggedIn(true)} />
+      )}
     </div>
   );
 };
